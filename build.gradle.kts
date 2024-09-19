479b858d8cc82a9d3715929e35c4b5d4eccb922e
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2024-09-20 02:33:28 +0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,62 @@
+group = "com.coffee-shop"
+version = "0.0.1-SNAPSHOT"
+description = "coffee-shop"
+val charset = "UTF-8"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 plugins {
     `java-library`
     `maven-publish`
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.ben.manes.versions)
+    alias(libs.plugins.version.catalog.update)
 }
 
-group = "com.coffee-shop"
-version = "0.0.1-SNAPSHOT"
-description = "coffee-shop"
-
-val charset = "UTF-8"
-
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-}
 
 repositories {
     mavenLocal()
     mavenCentral()
+    maven("https://repo.spring.io/release")
 }
 
 dependencies {
     // Spring Boot
-    implementation(libs.spring.boot.starter.web)
-    implementation(libs.spring.boot.starter.data.jpa)
-    implementation(libs.spring.boot.starter.validation)
-
+    implementation(libs.spring.boot.web)
+    implementation(libs.spring.boot.data.jpa)
+    implementation(libs.spring.boot.validation)
+    implementation(libs.spring.boot.actuator)
+    implementation(libs.spring.boot.configuration.processor)
     // OpenAPI
     implementation(libs.springdoc.openapi)
-
     // Database
     runtimeOnly(libs.postgresql)
-
     // MapStruct
     implementation(libs.mapstruct)
-
+    annotationProcessor(libs.mapstruct.processor)
     // Lombok
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
     annotationProcessor(libs.lombok.mapstruct.binding)
-
     // Test
-    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.test)
     testCompileOnly(libs.lombok)
     testAnnotationProcessor(libs.lombok)
+}
+
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.withType<JavaCompile> {
@@ -53,12 +65,4 @@ tasks.withType<JavaCompile> {
 
 tasks.withType<Javadoc> {
     options.encoding = charset
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
 }
